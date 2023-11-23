68da07cb9edefc953ab050c4f86cb6ced18aa873
kanaka308 <72907785+kanaka308@users.noreply.github.com>
2023-11-23 20:03:12 +0530

    little alterations

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 const Footer = () => {
   return (
-    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'><Link className='font-bold text-xl' href="/">MASSIMO</Link>
+    <div className='h-12 md:h-24 p-4 lg:p-4 xl:p-6 text-red-500 flex items-center justify-between'><Link className='font-bold text-xl' href="/">MASSIMO</Link>
     <p>@ ALL RIGHTS RESERVED</p>
     </div>
   )
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import CountDown from './CountDown'
 const Offer = () => {
   return (
-    <div className='bg-black h-screen flex flex-col md:flex-row'>
+    <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh]">
       {/* Text Container */}
       <div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
         <h1 className='text-white text-5xl font-bold xl:text-6xl'>Delicious Burger & French Fry</h1>
@@ -13,7 +13,7 @@ const Offer = () => {
         <button className="bg-red-500 text-white rounded-md py-3 px-6">Order Now</button>
       </div>
       {/* Image Container */}
-      <div className="flex-1 w-full relative">
+      <div className="flex-1 w-full relative md:h-full">
         <Image src="/offerProduct.png" alt="" fill className='object-contain'/>
       </div>
     </div>
